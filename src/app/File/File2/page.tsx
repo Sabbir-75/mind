abe37d84f1a19b23f89a7c6c79cb882bff6360c0
Sabbir-75 <mdsabbirhossain9200@gmail.com>
2026-01-10 22:06:16 +0600

    router part practice complete

---
+++ src/app/File/File2/page.tsx
@@ -0,0 +1,21 @@
+"use client"
+import { Menubar, MenubarMenu, MenubarTrigger } from '@/components/ui/menubar';
+import { useRouter } from 'next/navigation';
+import React from 'react';
+
+const page = () => {
+    const router = useRouter()
+    return (
+        <div>
+       <h1>File 2</h1>
+            <Menubar>
+                <MenubarMenu>
+                    <MenubarTrigger onClick={() => router.push("/File")} className="cursor-pointer">Go to File</MenubarTrigger>
+                </MenubarMenu>
+            </Menubar>
+        </div>
+
+    );
+};
+
+export default page;
--- src/app/File/page.tsx
+++ src/app/File/page.tsx
@@ -1,9 +1,25 @@
+"use client"
+import { Menubar, MenubarMenu, MenubarTrigger } from '@/components/ui/menubar';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const page = () => {
+    const router = useRouter()
     return (
         <div>
             <h1>File</h1>
+            <Menubar>
+                <MenubarMenu>
+                    <MenubarTrigger onClick={() => router.push("/File/File1")} className="cursor-pointer">File 1</MenubarTrigger>
+                </MenubarMenu>
+                <MenubarMenu>
+                    <MenubarTrigger onClick={() => router.push("/File/File2")} className="cursor-pointer">File 2</MenubarTrigger>
+                </MenubarMenu>
+                <MenubarMenu>
+                    <MenubarTrigger onClick={() => router.push("/")} className="cursor-pointer">Go to Home page</MenubarTrigger>
+                </MenubarMenu>
+            </Menubar>
+
         </div>
     );
 };
