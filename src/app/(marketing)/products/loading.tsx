d76cb832aac8900d27bbc0e3677124ca4a643d59
Sabbir-75 <mdsabbirhossain9200@gmail.com>
2026-02-08 03:55:57 +0600

    loading page create complete

---
+++ src/app/(marketing)/products/loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const loading = () => {
+    return (
+        <div className=' flex justify-center items-center pt-12'>
+            <span className="loading loading-bars loading-xl"></span>
+            <span className="loading loading-bars loading-xl"></span>
+        </div>
+    );
+};
+
+export default loading;
--- src/app/(marketing)/products/page.tsx
+++ src/app/(marketing)/products/page.tsx
@@ -1,53 +1,58 @@
 import Navigate from '@/components/common/Navigate/Navigate';
 import React from 'react';
 
-const page = () => {
+const page = async () => {
+    await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Resolve is Running");
+        }, 3000)
+    })
     return (
         <div className='flex flex-col items-center gap-3'>
-           <Navigate>1</Navigate>
-           <Navigate>2</Navigate>
-           <Navigate>3</Navigate>
-           <Navigate>4</Navigate>
-           <Navigate>5</Navigate>
-           <Navigate>3</Navigate>
-           <Navigate>4</Navigate>
-           <Navigate>5</Navigate>
-           <Navigate>3</Navigate>
-           <Navigate>4</Navigate>
-           <Navigate>5</Navigate>
-           <Navigate>3</Navigate>
-           <Navigate>4</Navigate>
-           <Navigate>5</Navigate>
-           <Navigate>3</Navigate>
-           <Navigate>4</Navigate>
-           <Navigate>5</Navigate>
-           <Navigate>3</Navigate>
-           <Navigate>4</Navigate>
-           <Navigate>5</Navigate>
-           <Navigate>3</Navigate>
-           <Navigate>4</Navigate>
-           <Navigate>5</Navigate>
-           <Navigate>3</Navigate>
-           <Navigate>4</Navigate>
-           <Navigate>5</Navigate>
-           <Navigate>3</Navigate>
-           <Navigate>4</Navigate>
-           <Navigate>5</Navigate>
-           <Navigate>3</Navigate>
-           <Navigate>4</Navigate>
-           <Navigate>5</Navigate>
-           <Navigate>3</Navigate>
-           <Navigate>4</Navigate>
-           <Navigate>5</Navigate>
-           <Navigate>3</Navigate>
-           <Navigate>4</Navigate>
-           <Navigate>5</Navigate>
-           <Navigate>3</Navigate>
-           <Navigate>4</Navigate>
-           <Navigate>5</Navigate>
-           <Navigate>3</Navigate>
-           <Navigate>4</Navigate>
-           <Navigate>5</Navigate>
+            <Navigate>1</Navigate>
+            <Navigate>2</Navigate>
+            <Navigate>3</Navigate>
+            <Navigate>4</Navigate>
+            <Navigate>5</Navigate>
+            <Navigate>3</Navigate>
+            <Navigate>4</Navigate>
+            <Navigate>5</Navigate>
+            <Navigate>3</Navigate>
+            <Navigate>4</Navigate>
+            <Navigate>5</Navigate>
+            <Navigate>3</Navigate>
+            <Navigate>4</Navigate>
+            <Navigate>5</Navigate>
+            <Navigate>3</Navigate>
+            <Navigate>4</Navigate>
+            <Navigate>5</Navigate>
+            <Navigate>3</Navigate>
+            <Navigate>4</Navigate>
+            <Navigate>5</Navigate>
+            <Navigate>3</Navigate>
+            <Navigate>4</Navigate>
+            <Navigate>5</Navigate>
+            <Navigate>3</Navigate>
+            <Navigate>4</Navigate>
+            <Navigate>5</Navigate>
+            <Navigate>3</Navigate>
+            <Navigate>4</Navigate>
+            <Navigate>5</Navigate>
+            <Navigate>3</Navigate>
+            <Navigate>4</Navigate>
+            <Navigate>5</Navigate>
+            <Navigate>3</Navigate>
+            <Navigate>4</Navigate>
+            <Navigate>5</Navigate>
+            <Navigate>3</Navigate>
+            <Navigate>4</Navigate>
+            <Navigate>5</Navigate>
+            <Navigate>3</Navigate>
+            <Navigate>4</Navigate>
+            <Navigate>5</Navigate>
+            <Navigate>3</Navigate>
+            <Navigate>4</Navigate>
+            <Navigate>5</Navigate>
         </div>
     );
 };
